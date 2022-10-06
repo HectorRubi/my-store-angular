bb5bddc08fc999fdd1acad286fb5a77be4857a28
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-10-06 11:45:53 -0500

    Add some properties and use the string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name: string = 'Nicolas';
+  age: number = 18;
+  img: string = 'https://source.unsplash.com/random'
 }
