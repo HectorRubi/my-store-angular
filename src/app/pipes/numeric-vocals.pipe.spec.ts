835432707422264b032f137f5355a02bf7dbe544
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-10-13 13:03:50 -0500

    Change product's title vocals to the relative numbers

---
+++ src/app/pipes/numeric-vocals.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NumericVocalsPipe } from './numeric-vocals.pipe';
+
+describe('NumericVocalsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NumericVocalsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/numeric-vocals.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'numericVocals'
+})
+export class NumericVocalsPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return value
+      .replace(/[a,A]/g, '4')
+      .replace(/[e,E]/g, '3')
+      .replace(/[i,I]/g, '1')
+      .replace(/[o,O]/g, '0')
+      .replace(/[u,U]/g, '6');
+  }
+
+}
