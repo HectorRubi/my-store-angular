60a2d7e4a1ee2a17b5b52cec6ae67026026881fd
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-10-13 14:09:17 -0500

    Add linter and use ng lint command

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from "../../models/product.model";
 
@@ -7,7 +7,7 @@ import { Product } from "../../models/product.model";
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
 
   @Input() product: Product = {
     id: '',
@@ -25,9 +25,6 @@ export class ProductComponent implements OnInit {
   @Output() addedProduct = new EventEmitter<Product>();
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   onAddToCart() {
     this.addedProduct.emit(this.product);
