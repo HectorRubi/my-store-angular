622a9ec5327b69fa7ce73eff8318c0fe82bced84
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-10-13 12:57:16 -0500

    Create two pipes and use date fns library

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -15,7 +15,7 @@ export class ProductsComponent implements OnInit {
   products: Product[] = [];
   myShoppingCart: Product[] = [];
   total = 0;
-  today = new Date(2021, 1, 10);
+  today = new Date(2020, 1, 10);
   date = new Date(2021, 2, 21);
 
   constructor(
