c9b3d3e30453142bcbe368145fcd82994393b812
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-10-17 16:13:23 -0500

    Create a user and get the access token to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +12,34 @@ export class AppComponent {
   imgParent = 'https://picsum.photos/200';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) { }
+
   onLoaded(img: string) {
     console.log('log padre', img);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService.create({
+      name: 'chip',
+      email: 'chiop@mailito.com',
+      password: 'qwerty12345',
+    })
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+
+  login() {
+    this.authService.login('chiop@mailito.com', 'qwerty12345')
+    .subscribe(rta => {
+      console.log(rta.access_token);
+    });
   }
 }
