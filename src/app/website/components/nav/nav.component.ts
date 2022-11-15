83e83b2c1dcec5b61d534df007db3f6f6fc6d304
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-11-14 18:48:50 -0600

    Create website directory and add components, directives, pipes and pages

--- src/app/website/components/nav/nav.component.ts
+++ src/app/website/components/nav/nav.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from "../../services/store.service";
-import { AuthService } from "../../services/auth.service";
-import { UsersService } from "../../services/users.service";
-import { TokenService } from "../../services/token.service";
+import { StoreService } from "../../../services/store.service";
+import { AuthService } from "../../../services/auth.service";
+import { UsersService } from "../../../services/users.service";
 
-import { Category } from "../../models/product.model";
+import { Category } from "../../../models/product.model";
 import { User } from 'src/app/models/user.model';
-import { CategoriesService } from "../../services/categories.service";
+import { CategoriesService } from "../../../services/categories.service";
 
 @Component({
   selector: 'app-nav',
