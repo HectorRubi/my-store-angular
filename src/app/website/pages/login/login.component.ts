4a3b8bb8416a28588fca3c064cd98cc5b7326692
Hector Rubi <hector.rubi@excent.capital>
2023-01-25 16:22:41 -0600

    Improve sign in form

--- src/app/website/pages/login/login.component.ts
+++ src/app/website/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    email: ['', [
+      Validators.required,
+      Validators.email,
+    ]],
+    password: ['', Validators.required],
+  });
+  hide = true;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit(): void {
+    console.log('hello world');
+  }
+
+  onSubmit() {
   }
 
 }
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwiperModule } from 'swiper/angular';
 import { QuicklinkModule } from 'ngx-quicklink';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -38,8 +41,11 @@ import { LayoutComponent } from './components/layout/layout.component';
     SwiperModule,
     SharedModule,
     QuicklinkModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class WebsiteModule { }
